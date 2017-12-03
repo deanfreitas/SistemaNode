ad59e77f1e7aa75b47b32635d79becd80be6a24d
Gilberto Freitas <gilberto.ademar@gmail.com>
2017-12-03 15:06:42 -0200

    refactor code

--- app/mongo/mongo-wrapper.js
+++ app/mongo/mongo-wrapper.js
@@ -2,7 +2,8 @@ const MongoClient = require('mongodb').MongoClient;
 
 const host = 'localhost';
 const port = '27017';
-const url = 'mongodb://' + host + ':' + port + '/integration_test';
+const db = 'iam';
+const url = 'mongodb://' + host + ':' + port + '/' + db;
 
 const connect = async () => {
     return await MongoClient.connect(url);
